3ebd00860f4bceab0cd41c83e691e7c4e88913d3
ML for health <135613724+MDKDP@users.noreply.github.com>
2025-10-18 10:21:52 +0800

    Update parser_rghat.py

--- code/RGHAT/parser_rghat.py
+++ code/RGHAT/parser_rghat.py
@@ -3,7 +3,7 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Run RGHAT.")
-    parser.add_argument('--epoch', type=int, default=10, help='Number of epoch.')
+    parser.add_argument('--epoch', type=int, default=100, help='Number of epoch.')
     parser.add_argument('--embed_size', type=int, default=768, help='Embedding size.')
     parser.add_argument('--kge_size', type=int, default=768, help='KG Embedding size.')
     parser.add_argument('--batch_size_kg', type=int, default=128, help='KG batch size.')
@@ -12,3 +12,4 @@ def parse_args():
     parser.add_argument('--temperature', type=float, default=0.5, help='Temperature coefficient.')
 
     return parser.parse_args()
+
